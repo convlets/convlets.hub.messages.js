a69e693d0abf4235f1ce7617c3add314d2fd856d
tolulope.bewaji <tolulope.bewaji@tricorde.net>
2015-06-15 00:16:32 +0100

    switched to using the simpler constructor-initialized fields rather than properties for messages

--- src/js/Register.js
+++ src/js/Register.js
@@ -1,32 +1,8 @@
 var Register = (function () {
-    function Register() {
+    function Register(id, username) {
+        this.id = id;
+        this.username = username;
+        this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
     }
-    Object.defineProperty(Register.prototype, "ID", {
-        get: function () {
-            return this.id;
-        },
-        set: function (value) {
-            this.id = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Register.prototype, "TypeID", {
-        get: function () {
-            return "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Register.prototype, "UserName", {
-        get: function () {
-            return this.username;
-        },
-        set: function (value) {
-            this.username = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
     return Register;
 })();
--- src/js/Register.ts
+++ src/js/Register.ts
@@ -1,22 +1,8 @@
 class Register
 {
-	private id: string;
-    public get ID() {
-        return this.id;
-    }
-    public set ID(value) {
-        this.id = value;
-    }
-
-    public get TypeID() {
-        return "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
-    }  
-
-	private username: string;
-    public get UserName() {
-        return this.username;
-    }
-    public set UserName(value) {
-        this.username = value;
-    }    
+    public typeID: string;       
+    constructor(public id: string, 
+        public username: string) {
+        this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
+    }   
 }
