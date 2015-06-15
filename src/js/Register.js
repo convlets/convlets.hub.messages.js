c38c74afdcbef816dddd1ae70e45cff06b56dd0e
tolulope.bewaji <tolulope.bewaji@tricorde.net>
2015-06-15 01:46:15 +0100

    added more fields for Register command

--- src/js/Register.js
+++ src/js/Register.js
@@ -1,7 +1,10 @@
 var Register = (function () {
-    function Register(id, username) {
+    function Register(id, username, password, phoneNumber, confirmedPhoneNumber) {
         this.id = id;
         this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.confirmedPhoneNumber = confirmedPhoneNumber;
         this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
     }
     return Register;
--- src/js/Register.ts
+++ src/js/Register.ts
@@ -2,7 +2,10 @@ class Register
 {
     public typeID: string;       
     constructor(public id: string, 
-        public username: string) {
+        public username: string,
+        public password: string,
+        public phoneNumber: string,
+        public confirmedPhoneNumber: string) {
         this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
     }   
 }
