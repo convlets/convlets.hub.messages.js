7ea9079acad92b2a2feed35192e3070138c6c007
tolulope.bewaji <tolulope.bewaji@tricorde.net>
2015-06-15 02:03:30 +0100

    initialized id in constructor

--- src/js/Register.js
+++ src/js/Register.js
@@ -1,10 +1,10 @@
 var Register = (function () {
-    function Register(id, username, phoneNumber, password, confirmedPassword) {
-        this.id = id;
+    function Register(username, phoneNumber, password, confirmedPassword) {
         this.username = username;
         this.phoneNumber = phoneNumber;
         this.password = password;
         this.confirmedPassword = confirmedPassword;
+        this.id = Guid.newGuid();
         this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
     }
     return Register;
--- src/js/Register.ts
+++ src/js/Register.ts
@@ -1,11 +1,13 @@
 class Register
 {
+	public id : string;
     public typeID: string;       
-    constructor(public id: string, 
-        public username: string,
+    constructor(public username: string,
         public phoneNumber: string,
         public password: string,
         public confirmedPassword: string) {
+
+    	this.id	 = Guid.newGuid();
         this.typeID = "CAE77047-222F-4FF0-86BB-CD2DD5D21ABF";
     }   
 }
